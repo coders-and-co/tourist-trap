dea0c621b37e48b35c7a4ea0156ac1dff26bdefa
Joshua Cender <joshua@sixthgear.ca>
2022-01-20 21:01:59 -0800

    Mouse movement

--- Player.cs
+++ Player.cs
@@ -3,9 +3,9 @@ using System;
 
 public class Player : KinematicBody2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+    [Export]
+    public int Speed = 5;
+    private Vector2? _target = null;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -15,28 +15,24 @@ public class Player : KinematicBody2D
 
     public override void _PhysicsProcess(float delta)
     {
-        
-        var d = new Vector2();
-        
-        if (Input.IsActionPressed("ui_left"))
+        if (_target.HasValue)
         {
-            d.x = -1;
+            var d = (Vector2) _target - Position;
+            if (d.Length() < Speed)
+            {
+                Position = (Vector2) _target;
+                _target = null;
+            }
+            else
+            {
+                Position += d.Normalized() * Speed;    
+            }
         }
-        else if (Input.IsActionPressed("ui_right"))
-        {
-            d.x = 1;
-        }
-        if (Input.IsActionPressed("ui_up"))
-        {
-            d.y = -1;
-        }
-        else if (Input.IsActionPressed("ui_down"))
-        {
-            d.y = 1;
-        }
-
-        Position += d.Normalized() * 5;
-        
+    }
+    
+    public void SetTarget(Vector2 t)
+    {
+        _target = t;
     }
     
 }
