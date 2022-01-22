ade0817ea9676a129dd516fec68295ce6d443c66
dentrala <andrew.jj.martin@gmail.com>
2022-01-22 13:12:47 -0800

    Added outlines to tourist

--- Game.cs
+++ Game.cs
@@ -4,13 +4,13 @@ using System;
 public class Game : Node2D
 {
 
-    // private Player _player;
-    
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        GD.Print("Hello from C#!");
-        // _player = GetNode<Player>("Player");
-    }
-    
+	// private Player _player;
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		GD.Print("Hello from C#!");
+		// _player = GetNode<Player>("Player");
+	}
+	
 }
