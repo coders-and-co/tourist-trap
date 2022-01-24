6f724e580646d58038c8f8d3b7663d6b5bd2fc72
Joshua Cender <joshua@sixthgear.ca>
2022-01-23 22:47:31 -0800

    Testing city!!

---
+++ assets/tilesets/TUNNEL.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="TUNNEL" tilewidth="64" tileheight="64" tilecount="256" columns="16">
+ <properties>
+  <property name="projectRoot" value="../.."/>
+ </properties>
+ <image source="../tiles/Road Spritesheet.png" width="1024" height="1024"/>
+</tileset>
