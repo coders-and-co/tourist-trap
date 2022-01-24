419c3dfb97ee0459cb56a5d745febe0734047124
Joshua Cender <joshua@sixthgear.ca>
2022-01-23 20:13:44 -0800

    fix game scene, more cars

--- entities/Traffic.cs
+++ entities/Traffic.cs
@@ -6,14 +6,24 @@ using Godot.Collections;
 [Tool]
 public class Traffic : Node2D
 {
-    public enum TrafficDirection {
+    public enum TrafficDirection
+    {
         Left,
         Right,
         Up,
         Down
     }
+
+    public string[] Colors = {
+        "blue", "red", "black", "white"
+    };
     
     private TrafficDirection _direction = TrafficDirection.Right;
+    private int _numberCarsAhead = 5;
+    private int _numberCarsBehind = 5;
+    private Array<AnimatedSprite> _cars = new Array<AnimatedSprite>();
+    // private Vector2 _respawnBoundary;
+    
     [Export]
     private TrafficDirection Direction
     {
@@ -24,34 +34,59 @@ public class Traffic : Node2D
     {
         _direction = dir;
         ClearCars();
-        SpawnCars(_direction, _numberCars);
+        SpawnCars();
     }
-    
-    private int _numberCars = 5;
-    [Export (PropertyHint.Range, "1,20,")] public int NumberCars {
-        get => _numberCars;
-        set => SetNumberCars(value);
+    [Export (PropertyHint.Range, "1,10,")] public int NumberCarsAhead {
+        get => _numberCarsAhead;
+        set => SetNumberCarsAhead(value);
     }
-    public void SetNumberCars(int num)
+    [Export (PropertyHint.Range, "1,10,")] public int NumberCarsBehind {
+        get => _numberCarsBehind;
+        set => SetNumberCarsBehind(value);
+    }
+    public void SetNumberCarsAhead(int num)
     {
-        _numberCars = num;
-        ClearCars();
-        SpawnCars(_direction, _numberCars);
+        _numberCarsAhead = num;
+        SpawnCars();
+    }
+    public void SetNumberCarsBehind(int num)
+    {
+        _numberCarsBehind = num;
+        SpawnCars();
+    }
+
+    public Vector2 GetTrafficVector()
+    {
+        switch (Direction)
+        {
+            case TrafficDirection.Left:
+                return Vector2.Left;
+            case TrafficDirection.Right:
+                return Vector2.Right;
+            case TrafficDirection.Up:
+                return Vector2.Up;
+            case TrafficDirection.Down:
+                return Vector2.Down;
+            default:
+                return Vector2.Zero;
+        }
+    }
+
+    public Vector2 GetRespawnPoint()
+    {
+        return GetTrafficVector() * _numberCarsBehind * -384;
     }
     
-    private Vector2 _respawnBoundary;
-    private Array<Node2D> _cars = new Array<Node2D>();
+    public Vector2 GetDespawnPoint()
+    {
+        return GetTrafficVector() * _numberCarsAhead * 384;
+    }
     
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        if (Engine.EditorHint)
-        {
-            
-        }
-        
-        // ClearCars();
-        SpawnCars(_direction, _numberCars);
+        // if (Engine.EditorHint)
+        SpawnCars();
     }
 
     public void ClearCars()
@@ -65,64 +100,61 @@ public class Traffic : Node2D
         _cars.Clear();
     }
 
-    public void SpawnCars(TrafficDirection dir, int num)
+    public void SpawnCars()
+    {
+        Vector2 spawnPos = GetRespawnPoint();
+        int totalCars = _numberCarsBehind + _numberCarsAhead;
+        
+        ClearCars();
+        
+        // Add new cars        
+        foreach (int value in Enumerable.Range(1, totalCars))
+        {
+            SpawnCar(spawnPos);
+            spawnPos += GetTrafficVector() * 384;
+        }
+    }
+
+    public void SpawnCar(Vector2 position = default)
     {
         PackedScene carScene;
-        Node2D carPrototype;
-        AnimatedSprite carPrototypeSprite;
-        Vector2 offset; // initial
-        Vector2 currentPosition = new Vector2(0, 0);
+        AnimatedSprite car;
+        
+        // pick a random color
+        string colorSuffix = "_" + Colors[GD.Randi() % Colors.Length];
         
         switch (Direction)
         {
             case TrafficDirection.Left:
                 carScene = GD.Load<PackedScene>("entities/CarHorizontal.tscn");
-                carPrototype = carScene.Instance<Node2D>();
-                carPrototypeSprite = carPrototype.GetNode<AnimatedSprite>("AnimatedSprite");
-                carPrototypeSprite.FlipH = true;
-                carPrototypeSprite.Play("running");
-                offset = new Vector2(-384, 0);
-                _respawnBoundary = new Vector2(Vector2.Left * _numberCars * 384);
+                car = carScene.Instance<AnimatedSprite>();
+                car.FlipH = true;
+                car.Play("running" + colorSuffix);
                 break;
             case TrafficDirection.Right:
                 carScene = GD.Load<PackedScene>("entities/CarHorizontal.tscn");
-                carPrototype = carScene.Instance<Node2D>();
-                carPrototypeSprite = carPrototype.GetNode<AnimatedSprite>("AnimatedSprite");
-                carPrototypeSprite.FlipH = false;
-                carPrototypeSprite.Play("running");
-                offset = new Vector2(384, 0);
-                _respawnBoundary = new Vector2(Vector2.Right * _numberCars * 384);
+                car = carScene.Instance<AnimatedSprite>();
+                car.FlipH = false;
+                car.Play("running" + colorSuffix);
                 break;
             case TrafficDirection.Up:
                 carScene = GD.Load<PackedScene>("entities/CarVertical.tscn");
-                carPrototype = carScene.Instance<Node2D>();
-                carPrototypeSprite = carPrototype.GetNode<AnimatedSprite>("AnimatedSprite");
-                carPrototypeSprite.Play("running_up");
-                offset = new Vector2(0, -384);
-                _respawnBoundary = new Vector2(Vector2.Up * _numberCars * 384);
+                car = carScene.Instance<AnimatedSprite>();
+                car.Play("running_up" + colorSuffix);
                 break;
             case TrafficDirection.Down:
                 carScene = GD.Load<PackedScene>("entities/CarVertical.tscn");
-                carPrototype = carScene.Instance<Node2D>();
-                carPrototypeSprite = carPrototype.GetNode<AnimatedSprite>("AnimatedSprite");
-                carPrototypeSprite.Play("running_down");
-                offset = new Vector2(0, 384);
-                _respawnBoundary = new Vector2(Vector2.Down * _numberCars * 384);
+                car = carScene.Instance<AnimatedSprite>();
+                car.Play("running_down" + colorSuffix);
                 break;
             default:
                 return;
         }
         
-        // Add new cars        
-        foreach (int value in Enumerable.Range(1, NumberCars))
-        {
-            var car = (Node2D) carPrototype.Duplicate();
-            GD.Print("Adding car: ", car);
-            car.Position = currentPosition;
-            AddChild(car); // Add car to scene
-            _cars.Add(car); // Add car to internal array
-            currentPosition += offset;
-        }
+        GD.Print("Adding car: ", car);
+        car.Position = position;
+        AddChild(car);
+        _cars.Add(car);
     }
 
     public override void _PhysicsProcess(float delta)
@@ -131,36 +163,44 @@ public class Traffic : Node2D
         {
             return;
         }
+
+        Vector2 despawnAt = GetDespawnPoint();
+        Vector2 respawnAt = GetRespawnPoint();
         
-        foreach (Node2D car in GetChildren())
+        foreach (AnimatedSprite car in _cars)
         {
+
+            car.Position += GetTrafficVector() * 10;
+            
             // car.Position = car.Position.LinearInterpolate()
             // Vector2 bounds;
-            switch (Direction)
-            {
-                case TrafficDirection.Left:
-                    car.Position += Vector2.Left * 10;
-                    if (car.Position.x <= _respawnBoundary.x)
-                        car.Position = Vector2.Zero;
-                    break;
-                case TrafficDirection.Right:
-                    car.Position += Vector2.Right * 10;
-                    if (car.Position.x >= _respawnBoundary.x)
-                        car.Position = Vector2.Zero;
-                    break;
-                case TrafficDirection.Up:
-                    car.Position += Vector2.Up * 10;
-                    if (car.Position.y <= _respawnBoundary.y)
-                        car.Position = Vector2.Zero;
-                    break;
-                case TrafficDirection.Down:
-                    car.Position += Vector2.Down * 10;
-                    if (car.Position.y >= _respawnBoundary.y)
-                        car.Position = Vector2.Zero;
-                    break;
-                default:
-                    return;
-            }
+            // switch (Direction)
+            // {
+            //     case TrafficDirection.Left:
+            //         car.Position += Vector2.Left * 10;
+            //         if (car.Position.x <= despawnAt.x)
+            //             car.QueueFree();
+            //             // car.Position = Vector2.Zero;
+            //         break;
+            //     case TrafficDirection.Right:
+            //         car.Position += Vector2.Right * 10;
+            //         if (car.Position.x >= despawnAt.x)
+            //             car.QueueFree();
+            //             // car.Position = Vector2.Zero;
+            //         break;
+            //     case TrafficDirection.Up:
+            //         car.Position += Vector2.Up * 10;
+            //         if (car.Position.y <= despawnAt.y)
+            //             car.Position = Vector2.Zero;
+            //         break;
+            //     case TrafficDirection.Down:
+            //         car.Position += Vector2.Down * 10;
+            //         if (car.Position.y >= despawnAt.y)
+            //             car.Position = Vector2.Zero;
+            //         break;
+            //     default:
+            //         return;
+            // }
         }
     }
 }
