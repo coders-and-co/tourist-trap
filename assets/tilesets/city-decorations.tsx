bf984b9fa1d12659db2a6f2b72f738e0ce1eba26
dentrala <andrew.jj.martin@gmail.com>
2022-01-30 15:57:08 -0800

    tileset/maps updated

---
+++ assets/tilesets/city-decorations.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="city-decorations" tilewidth="64" tileheight="64" tilecount="324" columns="18">
+ <image source="../tiles/City Decoration Spritesheet.png" width="1152" height="1152"/>
+ <tile id="28">
+  <objectgroup draworder="index" id="4">
+   <object id="3" x="16" y="31" width="52" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="29">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-7" y="22" width="92" height="39"/>
+  </objectgroup>
+ </tile>
+ <tile id="30">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="0" y="28" width="64" height="35"/>
+  </objectgroup>
+ </tile>
+</tileset>
