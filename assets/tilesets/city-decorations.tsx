d89af073274ca8cf5b8ffaca79839fb25ebba258
Erin <erin.liz.baird@gmail.com>
2022-01-30 20:23:34 -0800

    final collisions

--- assets/tilesets/city-decorations.tsx
+++ assets/tilesets/city-decorations.tsx
@@ -9,32 +9,44 @@
  <image source="../tiles/City Decoration Spritesheet.png" width="1152" height="1152"/>
  <tile id="28">
   <objectgroup draworder="index" id="4">
-   <object id="3" x="16" y="31" width="52" height="31"/>
+   <object id="4" x="45.2951" y="30.7496">
+    <polygon points="0,0 -12.2488,-30.2392 -22.7113,-24.1148 -24.37,-9.31419 -21.6906,-0.255183"/>
+   </object>
+   <object id="5" x="25.9011" y="33.3014">
+    <polygon points="0,0 -0.637959,26.6667 31.26,30.8772 35.7257,20.0319 35.2153,9.1866 31.7703,4.08293 27.3046,0.637959 18.5008,-0.382775 19.0112,-2.55183 -3.31738,-3.57257"/>
+   </object>
   </objectgroup>
  </tile>
  <tile id="29">
   <objectgroup draworder="index" id="2">
    <object id="1" x="-7" y="22" width="92" height="39"/>
+   <object id="3" x="7.91069" y="4.84848" width="52.4402" height="16.2041"/>
   </objectgroup>
  </tile>
  <tile id="30">
   <objectgroup draworder="index" id="3">
-   <object id="2" x="0" y="28" width="64" height="35"/>
+   <object id="3" x="4.72089" y="33.0463">
+    <polygon points="0,0 16.8421,-1.40351 25.6459,-29.0909 28.5805,-31.1324 33.6842,-27.6874 36.236,-17.9904 37.6396,-6.63477 36.874,3.0622 33.0463,10.9729 27.6874,14.2903 25.5183,25.9011 28.0702,29.2185 24.8804,31.26 19.1388,30.8772 17.6077,29.4737 14.1627,28.453 11.3557,28.3254 5.10367,28.9633 -0.76555,31.0048 -1.5311,26.9219 -2.04147,22.8389 -2.80702,21.4354 -4.46571,19.5215 -2.80702,4.72089"/>
+   </object>
   </objectgroup>
  </tile>
  <tile id="101">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="64" height="64"/>
+   <object id="2" x="27.9426" y="6.88995" width="34.7049" height="57.1611"/>
   </objectgroup>
  </tile>
  <tile id="119">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="64" height="64"/>
+   <object id="2" x="1.02073" y="1.02073" width="37.512" height="52.4402"/>
   </objectgroup>
  </tile>
  <tile id="137">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="64" height="64"/>
+   <object id="2" x="33.8118" y="9.69697" width="6.50718" height="53.0781"/>
+   <object id="3" x="33.3014" y="0.637959" width="5.99681" height="8.29346"/>
   </objectgroup>
  </tile>
 </tileset>
