429a88fae32f28c88cc90ed65a920a7ccfe4be81
dentrala <andrew.jj.martin@gmail.com>
2022-01-30 16:25:19 -0800

    poles and trees added

--- entities/Player.cs
+++ entities/Player.cs
@@ -5,71 +5,71 @@ using Duality.states.player;
 
 public class Player : KinematicBody2D, IEntity
 {
-    // Resources and nodes
-    private PackedScene _flagScene = GD.Load<PackedScene>("entities/Flag.tscn");
-    public Area2D InfluenceArea;
-    public Area2D InteractArea;
-    public Node2D Sprites;
-    public AnimatedSprite BodySprite;
-    public Sprite FlagSprite;
-    public AudioStreamPlayer2D Audio;
-    
-    // State variables
-    public FiniteStateMachine<Player> StateMachine;
-    public bool HasFlag = true;
-    
-    // Tunables
-    [Export] public int Speed = 240;
-    [Export] public int Influence = 50;
-    int IEntity.Influence { get => Influence; }
-    
-    public override void _Ready()
-    {
-        // lookup node references
-        InfluenceArea = GetNode<Area2D>("Influence");
-        InteractArea = GetNode<Area2D>("Interact");
-        Sprites = GetNode<Node2D>("Sprites");
-        BodySprite = GetNode<AnimatedSprite>("Sprites/Body");
-        FlagSprite = GetNode<Sprite>("Sprites/Flag");
-        Audio = GetNode<AudioStreamPlayer2D>("AudioStreamPlayer2D");
-        
-        // create state machine
-        StateMachine = new FiniteStateMachine<Player>(this, new PlayerIdleState());
+	// Resources and nodes
+	private PackedScene _flagScene = GD.Load<PackedScene>("entities/Flag.tscn");
+	public Area2D InfluenceArea;
+	public Area2D InteractArea;
+	public Node2D Sprites;
+	public AnimatedSprite BodySprite;
+	public Sprite FlagSprite;
+	public AudioStreamPlayer2D Audio;
+	
+	// State variables
+	public FiniteStateMachine<Player> StateMachine;
+	public bool HasFlag = true;
+	
+	// Tunables
+	[Export] public int Speed = 240;
+	[Export] public int Influence = 50;
+	int IEntity.Influence { get => Influence; }
+	
+	public override void _Ready()
+	{
+		// lookup node references
+		InfluenceArea = GetNode<Area2D>("Influence");
+		InteractArea = GetNode<Area2D>("Interact");
+		Sprites = GetNode<Node2D>("Sprites");
+		BodySprite = GetNode<AnimatedSprite>("Sprites/Body");
+		FlagSprite = GetNode<Sprite>("Sprites/Flag");
+		Audio = GetNode<AudioStreamPlayer2D>("AudioStreamPlayer2D");
+		
+		// create state machine
+		StateMachine = new FiniteStateMachine<Player>(this, new PlayerIdleState());
 
-        // signals
-        InteractArea.Connect("body_entered", this, "OnTouchSomething");
-        InteractArea.Connect("area_entered", this, "OnTouchSomething");
-    }
-    
-    public override void _PhysicsProcess(float delta)
-    {
-        StateMachine.Update(delta);
-    }
+		// signals
+		InteractArea.Connect("body_entered", this, "OnTouchSomething");
+		InteractArea.Connect("area_entered", this, "OnTouchSomething");
+	}
+	
+	public override void _PhysicsProcess(float delta)
+	{
+		StateMachine.Update(delta);
+	}
 
-    public override void _Input(InputEvent @event)
-    {
-        if (@event is InputEventMouseButton mouseEvent && mouseEvent.Pressed)
-        {
-            if ((ButtonList) mouseEvent.ButtonIndex == ButtonList.Left) {
-                GD.Print("Click at: ", GetGlobalMousePosition());
-                StateMachine.OnLeftClick(GetGlobalMousePosition());
-            }
-        }
-    }
-    
-    public Vector2 GetMovementVector()
-    {
-        Vector2 movement = Vector2.Zero;
-        if (Input.IsActionPressed("move_left"))
-            movement.x = -1;
-        else if (Input.IsActionPressed("move_right"))
-            movement.x = 1;
-        if (Input.IsActionPressed("move_up"))
-            movement.y = -1;
-        else if (Input.IsActionPressed("move_down"))
-            movement.y = 1;
-        return movement;
-    }
+	public override void _Input(InputEvent @event)
+	{
+		if (@event is InputEventMouseButton mouseEvent && mouseEvent.Pressed)
+		{
+			if ((ButtonList) mouseEvent.ButtonIndex == ButtonList.Left) {
+				GD.Print("Click at: ", GetGlobalMousePosition());
+				StateMachine.OnLeftClick(GetGlobalMousePosition());
+			}
+		}
+	}
+	
+	public Vector2 GetMovementVector()
+	{
+		Vector2 movement = Vector2.Zero;
+		if (Input.IsActionPressed("move_left"))
+			movement.x = -1;
+		else if (Input.IsActionPressed("move_right"))
+			movement.x = 1;
+		if (Input.IsActionPressed("move_up"))
+			movement.y = -1;
+		else if (Input.IsActionPressed("move_down"))
+			movement.y = 1;
+		return movement;
+	}
 
 	public void OnTouchSomething(Node body)
 	{
@@ -81,53 +81,53 @@ public class Player : KinematicBody2D, IEntity
 		}
 	}
 
-    public void Interact()
-    {
-        foreach (Node2D body in InteractArea.GetOverlappingBodies())
-        {
-            switch (body)
-            {
-                case var b when b.IsInGroup("Lights"):
-                    var traffic = GetTree().Root.GetNode<Traffic>("Game/Entities/Cars/TrafficFlow");
-                    traffic.Toggle();
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
-    
+	public void Interact()
+	{
+		foreach (Node2D body in InteractArea.GetOverlappingBodies())
+		{
+			switch (body)
+			{
+				case var b when b.IsInGroup("Lights"):
+					var traffic = GetTree().Root.GetNode<Traffic>("Game/Entities/Cars/TrafficFlow");
+					traffic.Toggle();
+					break;
+				default:
+					break;
+			}
+		}
+	}
+	
 
-    public void Shout()
-    {
-        var inf = InfluenceArea.GetChild<CollisionShape2D>(0); // .Shape;
-        Tween tween = new Tween();
-        GetTree().Root.AddChild(tween);
-        tween.InterpolateProperty(inf, "shape:radius", 32, 256, 1.0f);
-        tween.InterpolateProperty(this, "Influence", 50, 150, 1.0f);
-        tween.InterpolateProperty(inf, "shape:radius", 256, 32, 5.0f, Tween.TransitionType.Linear, Tween.EaseType.InOut, 2.5f);
-        tween.InterpolateProperty(this, "Influence", 150, 50, 5.0f, Tween.TransitionType.Linear, Tween.EaseType.InOut, 2.5f);
-        tween.Start();
-    }
+	public void Shout()
+	{
+		var inf = InfluenceArea.GetChild<CollisionShape2D>(0); // .Shape;
+		Tween tween = new Tween();
+		GetTree().Root.AddChild(tween);
+		tween.InterpolateProperty(inf, "shape:radius", 32, 256, 1.0f);
+		tween.InterpolateProperty(this, "Influence", 50, 150, 1.0f);
+		tween.InterpolateProperty(inf, "shape:radius", 256, 32, 5.0f, Tween.TransitionType.Linear, Tween.EaseType.InOut, 2.5f);
+		tween.InterpolateProperty(this, "Influence", 150, 50, 5.0f, Tween.TransitionType.Linear, Tween.EaseType.InOut, 2.5f);
+		tween.Start();
+	}
 
-    public void ThrowFlag(Vector2 to)
-    {
-        GD.Print("Throwing flag to ", to);
-        HasFlag = false;
-        
-        // Add Flag to Game 
-        Flag flag = _flagScene.Instance<Flag>();
-        var entities = GetTree().Root.GetNode<Node2D>("Game/Entities");
-        entities.AddChild(flag);
-        // Call Throw method for flag
-        flag.FlyBetween(Position, to);
-    }
+	public void ThrowFlag(Vector2 to)
+	{
+		GD.Print("Throwing flag to ", to);
+		HasFlag = false;
+		
+		// Add Flag to Game 
+		Flag flag = _flagScene.Instance<Flag>();
+		var entities = GetTree().Root.GetNode<Node2D>("Game/Entities");
+		entities.AddChild(flag);
+		// Call Throw method for flag
+		flag.FlyBetween(Position, to);
+	}
 
-    public void CollectFlag(Flag flagNode)
-    {
-        HasFlag = true;
-        FlagSprite.Visible = true;
-        flagNode.QueueFree();
-    }
-    
+	public void CollectFlag(Flag flagNode)
+	{
+		HasFlag = true;
+		FlagSprite.Visible = true;
+		flagNode.QueueFree();
+	}
+	
 }
--- states/tourist/TouristMeanderState.cs
+++ states/tourist/TouristMeanderState.cs
@@ -2,43 +2,43 @@ using Godot;
 
 namespace Duality.states.tourist
 {
-    public class TouristMeanderState : BaseState<Tourist>
-    {
-        public override string GetName() { return "Meander"; }
-        private float _timer;
-        private Vector2 _force;
-        public Vector2 Force { get => _force; }
-        public override void OnEnter()
-        {
-            _timer = (float) GD.RandRange(1, 2); // 1 to 2 seconds
-            _force = new Vector2(RefObj.Speed, 0);
-            _force = _force.Rotated(GD.Randf() * Mathf.Pi * 2); 
-            RefObj.BodySprite.Play("walk");
-            RefObj.Vision.Connect("body_entered", this, "Spotted");
-            RefObj.Vision.Connect("area_entered", this, "Spotted");
-        }
+	public class TouristMeanderState : BaseState<Tourist>
+	{
+		public override string GetName() { return "Meander"; }
+		private float _timer;
+		private Vector2 _force;
+		public Vector2 Force { get => _force; }
+		public override void OnEnter()
+		{
+			_timer = (float) GD.RandRange(1, 2); // 1 to 2 seconds
+			_force = new Vector2(RefObj.Speed, 0);
+			_force = _force.Rotated(GD.Randf() * Mathf.Pi * 2); 
+			RefObj.BodySprite.Play("walk");
+			RefObj.Vision.Connect("body_entered", this, "Spotted");
+			RefObj.Vision.Connect("area_entered", this, "Spotted");
+		}
 
-        public void Spotted(Node2D thing) 
-        {
-            // when we spot a new potential target, instantly check it
-            _timer = 0;
-        }
+		public void Spotted(Node2D thing) 
+		{
+			// when we spot a new potential target, instantly check it
+			_timer = 0;
+		}
 
-        public override BaseState<Tourist>? Update(float delta)
-        {
-            if (_timer <= 0)
-            {
-                var (target, score) = RefObj.FindTarget(); 
-                if (target != null && Map.BusTakeMeHome && target.Name == "Bus")
-                    return new TouristLoadBusState(target);
-                if (target != null && score > RefObj.MinFollowScore && target.Position.DistanceTo(RefObj.Position) > RefObj.ComfortDistance && target.Name != "Bus")
-                    return new TouristFollowState(target, score);
-                else
-                    return new TouristIdleState();
-            }
-            _force = _force.Rotated(GD.Randf() * 0.2f - 0.1f); // add warble
-            _timer -= delta;
-            return null;
-        }
-    }
+		public override BaseState<Tourist>? Update(float delta)
+		{
+			if (_timer <= 0)
+			{
+				var (target, score) = RefObj.FindTarget(); 
+				if (target != null && Map.BusTakeMeHome && target.Name == "Bus")
+					return new TouristLoadBusState(target);
+				if (target != null && score > RefObj.MinFollowScore && target.Position.DistanceTo(RefObj.Position) > RefObj.ComfortDistance && target.Name != "Bus")
+					return new TouristFollowState(target, score);
+				else
+					return new TouristIdleState();
+			}
+			_force = _force.Rotated(GD.Randf() * 0.2f - 0.1f); // add warble
+			_timer -= delta;
+			return null;
+		}
+	}
 }
